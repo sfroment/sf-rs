a764f2cd9a71644074e756001a912a67494636b5
Sacha Froment <sfroment42@gmail.com>
2025-05-01 11:15:50 +0200

    chore: add some stuff
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- sf-wasm-2/src/wasm.rs
+++ sf-wasm-2/src/wasm.rs
@@ -1,17 +1,18 @@
-use futures::{SinkExt, Stream, StreamExt, channel::mpsc, stream::SplitStream};
+use futures::{SinkExt, Stream, StreamExt, channel::mpsc};
 use gloo_console::log;
 use gloo_net::websocket::{Message, WebSocketError, futures::WebSocket};
-use serde_json::value::RawValue;
 use sf_peer_id::PeerID;
 use sf_protocol::{PeerEvent, PeerRequest};
-use std::{cell::RefCell, rc::Rc, sync::Arc};
+use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 
 pub struct Client {
     peer_id: PeerID,
     on_message_callbacks: RefCell<Vec<js_sys::Function>>,
+    on_new_peer_callbacks: RefCell<Vec<js_sys::Function>>,
     sender: RefCell<Option<mpsc::Sender<Message>>>,
+    peers: RefCell<Vec<PeerID>>,
 }
 
 impl Client {
@@ -20,7 +21,9 @@ impl Client {
         Ok(Rc::new(Self {
             peer_id,
             on_message_callbacks: RefCell::new(Vec::new()),
+            on_new_peer_callbacks: RefCell::new(Vec::new()),
             sender: RefCell::new(None),
+            peers: RefCell::new(Vec::new()),
         }))
     }
 
@@ -37,6 +40,18 @@ impl Client {
 
     pub fn remove_on_message(self: &mut Rc<Self>, id: usize) {
         let mut callbacks = self.on_message_callbacks.borrow_mut();
+        callbacks.remove(id);
+    }
+
+    pub fn on_new_peer(self: &mut Rc<Self>, callback: js_sys::Function) -> usize {
+        let mut callbacks = self.on_new_peer_callbacks.borrow_mut();
+        let id = callbacks.len();
+        callbacks.push(callback);
+        id
+    }
+
+    pub fn remove_on_new_peer(self: &mut Rc<Self>, id: usize) {
+        let mut callbacks = self.on_new_peer_callbacks.borrow_mut();
         callbacks.remove(id);
     }
 
@@ -77,11 +92,35 @@ impl Client {
                                 peer_request.clone()
                             );
 
-                            let peer_request_js =
-                                serde_wasm_bindgen::to_value(&peer_request).unwrap();
-                            for callback in client.on_message_callbacks.borrow().iter() {
-                                let this = JsValue::NULL;
-                                let _ = callback.call1(&this, &peer_request_js);
+                            match peer_request {
+                                PeerRequest::Forward { data, .. } => match data {
+                                    PeerEvent::NewPeer { peer_id } => {
+                                        client.peers.borrow_mut().push(peer_id);
+                                        for callback in client.on_new_peer_callbacks.borrow().iter()
+                                        {
+                                            let this = JsValue::NULL;
+                                            let peer_id_js =
+                                                serde_wasm_bindgen::to_value(&peer_id.to_string())
+                                                    .unwrap();
+                                            let _ = callback.call1(&this, &peer_id_js);
+                                        }
+                                    }
+                                    PeerEvent::Message { peer_id, message } => {
+                                        for callback in client.on_message_callbacks.borrow().iter()
+                                        {
+                                            let this = JsValue::NULL;
+                                            let message_js =
+                                                serde_wasm_bindgen::to_value(&message).unwrap();
+                                            let peer_id_js =
+                                                serde_wasm_bindgen::to_value(&peer_id.to_string())
+                                                    .unwrap();
+                                            let _ = callback.call2(&this, &peer_id_js, &message_js);
+                                        }
+                                    }
+                                },
+                                _ => {
+                                    log!("unhandled event type: {:?}", peer_request.clone());
+                                }
                             }
                         }
                     }
@@ -97,7 +136,7 @@ impl Client {
         let mut sender = self.sender.borrow_mut();
         if let Some(sender) = sender.as_mut() {
             let text = serde_json::to_string(&peer_request)
-                .map_err(|e| JsError::new(&format!("Failed to serialize peer request: {}", e)))?;
+                .map_err(|e| JsError::new(&format!("Failed to serialize peer request: {e}")))?;
             let message = Message::Text(text);
             let mut sender = sender.clone();
             spawn_local(async move {
@@ -109,6 +148,10 @@ impl Client {
         } else {
             Err(JsError::new("WebSocket not connected"))
         }
+    }
+
+    fn list_peers(&self) -> Vec<PeerID> {
+        self.peers.borrow().clone()
     }
 }
 
@@ -128,6 +171,15 @@ impl ClientWrapper {
     #[wasm_bindgen(getter)]
     pub fn peer_id(&self) -> String {
         self.client.peer_id().to_string()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn list_peers(&self) -> Vec<String> {
+        log!("list_peers: {:?}", self.client.list_peers().len());
+        let peers = self.client.list_peers();
+        let peers_str: Vec<String> = peers.iter().map(|p| p.to_string()).collect();
+        log!("peers_str: {:?}", peers_str.clone());
+        peers_str
     }
 
     pub fn connect(&self, url: &str) -> Result<(), JsError> {
@@ -155,19 +207,11 @@ impl ClientWrapper {
         self.client.send_peer_request(peer_request)
     }
 
-    //fn list_peers(&self) -> Vec<PeerID> {
-    //    unimplemented!();
-    //}
+    pub fn on_new_peer(&mut self, callback: js_sys::Function) -> usize {
+        self.client.on_new_peer(callback)
+    }
 
-    //fn send_to_peer(&self, peer_id: PeerID, message: &[u8]) {
-    //    unimplemented!();
-    //}
-
-    //fn send_to_room(&self, room_id: &str, message: &[u8]) {
-    //    unimplemented!();
-    //}
-
-    //fn register_event_callback(&self, event_name: &str, callback: Function) {
-    //    unimplemented!();
-    //}
+    pub fn remove_on_new_peer(&mut self, id: usize) {
+        self.client.remove_on_new_peer(id);
+    }
 }
